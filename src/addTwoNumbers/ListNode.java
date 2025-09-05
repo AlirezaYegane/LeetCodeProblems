74ba0639354fd17aa4e8b0437df48cbb9d2474b8
AlirezaYegane <alireza.yegane.cs@gmail.com>
2025-09-05 21:51:31 +0330

    add two numbers problem

---
+++ src/addTwoNumbers/ListNode.java
@@ -0,0 +1,29 @@
+package addTwoNumbers;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public  ListNode(int val) {
+        this.val = val;
+    }
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+    public  ListNode() {
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        ListNode cur = this;
+        while (cur != null) {
+            sb.append(cur.val);
+            if (cur.next != null) sb.append(", ");
+            cur = cur.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+}
