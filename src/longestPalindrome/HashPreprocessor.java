459831db39142d05b8fa5483064b9b0b2f28f284
AlirezaYegane <alireza.yegane.cs@gmail.com>
2025-09-11 23:12:34 +0330

    HashPreprocessor done

---
+++ src/longestPalindrome/HashPreprocessor.java
@@ -0,0 +1,18 @@
+package longestPalindrome;
+
+public class HashPreprocessor implements Preprocessor {
+
+    @Override
+    public char[] transform(String s) {
+        char[] res = new char[2*s.length() +3];
+        int idx = 0;
+        res [idx++] ='^';
+        for (int i =0;i<s.length();i++) {
+            res [idx++]= '#';
+            res [idx++]=s.charAt(i);
+        }
+        res [idx++]='#';
+        res [idx++]='$';
+        return res;
+    }
+}
