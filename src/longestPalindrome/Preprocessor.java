dead4706fd96ed2c4a098a2a981078b4bf3c31d2
AlirezaYegane <alireza.yegane.cs@gmail.com>
2025-09-12 00:07:11 +0330

    Preprocessor updated

--- src/longestPalindrome/Preprocessor.java
+++ src/longestPalindrome/Preprocessor.java
@@ -1,6 +1,8 @@
 package longestPalindrome;
 
 public interface Preprocessor {
-    /** Like : "babad" -> "^#b#a#b#a#d#$" */
+    /**
+     * Like : "babad" -> "^#b#a#b#a#d#$"
+     */
     char[] transform(String s);
 }
