2a13fe3b4ea4dad076b30da3d693d91b113956b2
a.yegane <alireza.yegane.cs@gmail.com>
2025-09-29 12:30:38 +0330

    TwoPointerThreeSumSolver added

---
+++ src/algorithm/threeSum/TwoPointerThreeSumSolver.java
@@ -0,0 +1,39 @@
+package algorithm.threeSum;
+
+import java.util.*;
+
+public class TwoPointerThreeSumSolver implements ThreeSumSolver {
+
+    @Override
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) return res;
+
+        Arrays.sort(nums);
+        final int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            if (nums[i] > 0) break;
+            if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break;
+            if (nums[i] + nums[n - 2] + nums[n - 1] < 0) continue;
+
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int s = nums[i] + nums[l] + nums[r];
+                if (s == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    int lv = nums[l], rv = nums[r];
+                    while (l < r && nums[l] == lv) l++;
+                    while (l < r && nums[r] == rv) r--;
+                } else if (s < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+}
